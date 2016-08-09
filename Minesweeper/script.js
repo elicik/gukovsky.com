80f0ec723e2caece488ba213a2401a91ce7ef639
Eli Gukovsky <gukovskye@gmail.com>
2016-08-09 01:09:10 -0400

    Almost complete mastermind functionality

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -7,6 +7,7 @@ var bombs = 99;
 var hints = false;
 
 var grid = [];
+var firstClick = true;
 
 var Cell = function() {
 	this.bomb = false;
@@ -58,22 +59,165 @@ function generateGrid() {
 	}
 }
 
+function generateHTML() {
+	var table = document.createElement("table");
+	table.id = "minesweeper";
+	for (var y = 0; y < height; y++) {
+		var tr = document.createElement("tr");
+		for (var x = 0; x < width; x++) {
+			var td = document.createElement("td");
+			td.dataset.x = x;
+			td.dataset.y = y;
+			td.addEventListener("click", cellLeftClick);
+			td.addEventListener("contextmenu", cellRightClick);
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+	document.body.insertBefore(table, document.body.firstChild);
+	updateAllHTML();
+}
 
-// console.table(grid.map(function(arr) {
-// 	return arr.map(function(obj) {
-// 		return obj.surrounding;
-// 	});
-// }));
-// console.table(grid.map(function(arr) {
-// 	return arr.map(function(obj) {
-// 		return obj.bomb;
-// 	});
-// }));
+function updateHTML(x, y) {
+	var td = document.querySelector('#minesweeper td[data-x="'+x+'"][data-y="'+y+'"]');
+	var cell = grid[x][y];
+	td.dataset.bomb = cell.bomb;
+	td.dataset.surrounding = cell.surrounding;
+	td.dataset.activated = cell.activated;
+	td.dataset.flagged = cell.flagged;
+	td.dataset.hint = cell.hint;
 
+	if (cell.activated && cell.surrounding >= 1) {
+		td.innerHTML = cell.surrounding;
+	}
+	else {
+		td.innerHTML = "";
+	}
+}
 
+function updateAllHTML() {
+	for (var x = 0; x < width; x++) {
+		for (var y = 0; y < height; y++) {
+			updateHTML(x, y);
+		}
+	}
+}
+
+function reveal(x, y) {
+	var cell = grid[x][y];
+	if (!cell.activated && !cell.flagged && !cell.hint) {
+		if (cell.bomb) {
+			gameOver();
+		}
+		else {
+			cell.activated = true;
+			if (cell.surrounding === 0) {
+				revealSurroundings(x, y);
+			}
+		}
+	}
+	updateHTML(x, y);
+	// TODO: check win
+}
+function revealSurroundings(x, y) {
+	for (var i = Math.max(x-1, 0); i <= Math.min(x+1, width - 1); i++) {
+		for (var j = Math.max(y-1, 0); j <= Math.min(y+1, height - 1); j++) {
+			reveal(i, j);
+		}
+	}
+}
+
+function gameOver() {
+	// TODO: revealAllBombs();
+	alert("YOU LOSE");
+}
+
+function prepareFirstClick(x, y) {
+	var bombs = 0;
+	// Add up # of bombs, set all bombs to prevent bomb placement
+	for (var i = Math.max(x-1, 0); i <= Math.min(x+1, width - 1); i++) {
+		for (var j = Math.max(y-1, 0); j <= Math.min(y+1, height - 1); j++) {
+			if (grid[i][j].bomb) {
+				bombs++;
+			}
+			else {
+				grid[i][j].bomb = true;
+			}
+		}
+	}
+	// Place bombs
+	for (var i = 0; i < bombs; i++) {
+		placeBomb();
+	}
+	// Remove bombs from first click
+	for (var i = Math.max(x-1, 0); i <= Math.min(x+1, width - 1); i++) {
+		for (var j = Math.max(y-1, 0); j <= Math.min(y+1, height - 1); j++) {
+			grid[i][j].bomb = false;
+		}
+	}
+	calculateSurroundings();
+	updateAllHTML();
+}
+
+function cellLeftClick(event) {
+	var td = event.target;
+	var x = parseInt(td.dataset.x, 10);
+	var y = parseInt(td.dataset.y, 10);
+
+	if (firstClick) {
+		prepareFirstClick(x, y);
+		firstClick = false;
+	}
+
+	var cell = grid[x][y];
+	if (cell.activated) {
+		var surroundingFlags = 0;
+		for (var i = Math.max(x-1, 0); i <= Math.min(x+1, width - 1); i++) {
+			for (var j = Math.max(y-1, 0); j <= Math.min(y+1, height - 1); j++) {
+				if (grid[i][j].flagged) {
+					surroundingFlags++;
+				}
+			}
+		}
+		if (cell.surrounding === surroundingFlags) {
+			revealSurroundings(x, y);
+		}
+	}
+	else {
+		reveal(x, y);
+	}
+}
+function cellRightClick(event) {
+	var td = event.target;
+	var x = parseInt(td.dataset.x, 10);
+	var y = parseInt(td.dataset.y, 10);
+	var cell = grid[x][y];
+
+	if (cell.activated) {
+		event.preventDefault();
+		return false;
+	}
+
+	if (hints) {
+		// !flag, !hint -> flag, !hint
+		// flag, !hint -> !flag, hint
+		// !flag, hint -> !flag, !hint
+		var hint = !cell.flagged;
+		var flagged = !cell.flagged && !cell.hint;
+		cell.hint = hint;
+		cell.flagged = flagged;
+	}
+	else {
+		cell.flagged = !cell.flagged;
+	}
+	updateHTML(x, y);
+
+	event.preventDefault();
+	return false;
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
 	generateGrid();
 	calculateSurroundings();
-	
+	generateHTML();
 });
