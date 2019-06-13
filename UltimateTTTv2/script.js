2b84a49c089514b226db86478f02601c7371f6f4
Eli Gukovsky <gukovskye@gmail.com>
2019-06-12 22:22:30 -0400

    UltimateTTTv2 complete

--- UltimateTTTv2/script.js
+++ UltimateTTTv2/script.js
@@ -1,9 +1,7 @@
 "use strict";
 
-// TODO: Keep past versions of grid and move
-
 function create_blank_grid(default_value) {
-	return new Array(3).map(() => new Array(3).map(() => new Array(3).map(() => new Array(3).fill(default_value))));
+	return new Array(3).fill().map(() => new Array(3).fill().map(() => new Array(3).fill().map(() => new Array(3).fill(default_value))));
 }
 
 var app = new Vue({
@@ -11,7 +9,7 @@ var app = new Vue({
 	data: {
 		turn: "X",
 		grid: create_blank_grid(""),
-		last_move: []
+		last_move: [],
 	},
 	computed: {
 		big_grid: function() {
@@ -40,15 +38,14 @@ var app = new Vue({
 				}
 			}
 			return result;
-		}
+		},
 		possible: function() {
-			// TODO: Return possible moves
-			if (this.last_move.length == 0) {
+			if (this.last_move.length === 0) {
 				return create_blank_grid(true);
 			}
-			else if (this.big_grid[last_move[2], last_move[3]] !== "") {
-				return this.grid.map(function(row, row_num) {
-					return row.map(function(col, col_num) {
+			if (this.big_grid[this.last_move[2]][this.last_move[3]] !== "") {
+				return this.grid.map((row, row_num) =>
+					row.map((col, col_num) => {
 						if (this.big_grid[row_num][col_num] !== "") {
 							return [[false, false, false], [false, false, false], [false, false, false]];
 						}
@@ -59,13 +56,13 @@ var app = new Vue({
 								});
 							});
 						}
-					});
-				});
+					})
+				);
 			}
 			else {
 				let result = create_blank_grid(false);
-				let row = last_move[2];
-				let col = last_move[3];
+				let row = this.last_move[2];
+				let col = this.last_move[3];
 				result[row][col] = this.grid[row][col].map(function(i) {
 					return i.map(function(j) {
 						return j === "";
@@ -74,122 +71,27 @@ var app = new Vue({
 				return result;
 			}
 		}
-	}
-})
-
-
-var turn = true; // true: Red, false: Blue
-var grid = [
-	[
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 0, 0
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 0, 1
-		[["", "", ""], ["", "", ""], ["", "", ""]]  // 0, 2
-	],
-	[
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 1, 0
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 1, 1
-		[["", "", ""], ["", "", ""], ["", "", ""]]  // 1, 2
-	],
-	[
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 2, 0
-		[["", "", ""], ["", "", ""], ["", "", ""]], // 2, 1
-		[["", "", ""], ["", "", ""], ["", "", ""]]  // 2, 2
-	]
-];
-
-function reset() {
-	turn = true;
-	grid = [[[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]]], [[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]] ], [[["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]], [["", "", ""], ["", "", ""], ["", "", ""]]]];
-	$(".Red").removeClass("Red");
-	$(".Blue").removeClass("Blue");
-	$("table table td").addClass("possible");
-	$("#turn").html("Red");
-	$("#turn").css("color", "red");
-}
-
-function tie() {
-	// Assuming winner() is false
-	return !this.big_grid.flat().some("");
-}
-
-function winner() {
-	for (let player of ["X", "O"]) {
-		for (let i = 0; i < 3; i++) {
-			// Horizontal
-			if (this.big_grid[i].every(x => x === player)) {
-				return player;
+	},
+	methods: {
+		play: function(row, col, i, j) {
+			// Use splice because editing the array directly does not trigger update
+			if (this.grid[row][col][i][j] !== "" || this.big_grid[row][col] !== "") {
+				return;
 			}
-			// Vertical
-			if ([this.big_grid[0][i], this.big_grid[1][i], this.big_grid[2][i]].every(x => x === player)) {
-				return player;
-			}
-		}
-		// Diagonals
-		if ([this.big_grid[0][0], this.big_grid[1][1], this.big_grid[2][2]].every(x => x === player)) {
-			return player;
-		}
-		if ([this.big_grid[0][2], this.big_grid[1][1], this.big_grid[2][0]].every(x => x === player)) {
-			return player;
-		}
-	}
-	return "";
-}
-
-$(document).ready(function() {
-	var innerhtml = "";
-	for(var row = 0; row < 3; row++) {
-		innerhtml += "<tr>";
-		for(var col = 0; col < 3; col++) {
-			innerhtml += "<td><table data-row=\"" + row + "\" data-column=\"" + col + "\">";
-			for(var i = 0; i < 3; i++) {
-				innerhtml += "<tr>";
-				for(var j = 0; j < 3; j++) {
-					innerhtml += "<td data-row=\"" + i + "\" data-column=\"" + j + "\"></td>";
-				}
-				innerhtml += "</tr>";
-			}
-			innerhtml += "</table></td>";
-		}
-		innerhtml += "</tr>";
-	}
-
-	$("#ultimate").html(innerhtml);
-	$("table table td").addClass("possible");
-
-	$("table table td").click(function() {
-		if($(this).hasClass("possible")) {
-			var row = Number($($(this).parents("table:not(#ultimate)")[0]).data("row"));
-			var col = Number($($(this).parents("table:not(#ultimate)")[0]).data("column"));
-			var i = Number($(this).data("row"));
-			var j = Number($(this).data("column"));
-			grid[row][col][i][j] = turn ? "Red" : "Blue";
-			$(".possible").removeClass("possible");
-			$(this).addClass(grid[row][col][i][j]);
-
-			if(winSquare(row, col)) { // Move won a square
-				grid[row][col] = turn ? "Red" : "Blue";
-				$("table table[data-row=" + row + "][data-column=" + col + "]").addClass(turn ? "Red" : "Blue");
-			}
-			if($.isArray(grid[row][col])) { // Tied square
-				if(grid[row][col][0].indexOf("") === -1 &&
-					grid[row][col][1].indexOf("") === -1 &&
-					grid[row][col][2].indexOf("") === -1) {
-					grid[row][col] = "";
-				}
-			}
-
-			if(winner()) { // Game winner
+			this.grid[row][col][i].splice(j, 1, this.turn);
+			this.last_move = [row, col, i, j];
+			if(this.winner()) { // Game winner
 				swal({
 					title: "Game over!",
-					text: (turn ? "Red" : "Blue") + " won! Congrats!",
+					text: `${this.turn}  won! Congrats!`,
 					icon: "success",
 					buttons: {
 						cancel: false,
 						confirm: "Play again"
 					}
-				}).then(reset);
+				}).then(this.reset);
 			}
-			else if(tie()) { // Game tied
+			else if(this.tie()) { // Game tied
 				swal({
 					title: "Game over!",
 					text: "There was a tie!",
@@ -198,19 +100,40 @@ $(document).ready(function() {
 						cancel: false,
 						confirm: "Play again"
 					}
-				}).then(reset);
+				}).then(this.reset);
 			}
-			else { // If square is available, make it possible. If not, make everything possible
-				if($.isArray(grid[i][j])) {
-					$("table table[data-row=" + i + "][data-column=" + j + "] td:not(.Red, .Blue)").addClass("possible");
+			this.turn = this.turn === "O" ? "X" : "O";
+
+		},
+		tie: function() {
+			// Assuming winner() is false
+			return !this.big_grid.flat().some(x => x === "");
+		},
+		winner: function() {
+			for (let player of ["X", "O"]) {
+				for (let i = 0; i < 3; i++) {
+					// Horizontal
+					if (this.big_grid[i].every(x => x === player)) {
+						return player;
+					}
+					// Vertical
+					if ([this.big_grid[0][i], this.big_grid[1][i], this.big_grid[2][i]].every(x => x === player)) {
+						return player;
+					}
 				}
-				else {
-					$("table table:not(.Red, .Blue) td:not(.Red, .Blue)").addClass("possible");
+				// Diagonals
+				if ([this.big_grid[0][0], this.big_grid[1][1], this.big_grid[2][2]].every(x => x === player)) {
+					return player;
 				}
-				$("#turn").html(turn ? "Blue" : "Red");
-				$("#turn").css("color", turn ? "blue" : "red");
+				if ([this.big_grid[0][2], this.big_grid[1][1], this.big_grid[2][0]].every(x => x === player)) {
+					return player;
+				}
 			}
-			turn = !turn;
+			return "";
+		},
+		reset: function() {
+			this.turn = "X";
+			this.grid = create_blank_grid("");
 		}
-	});
-});
+	}
+})
